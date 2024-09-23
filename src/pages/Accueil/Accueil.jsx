cc420ef1994e55148766652a421ff6bd27c28a60
Your Name <you@example.com>
2024-09-23 10:02:14 +0200

    Correction et mise en place du Header et de la banniere plus ajout page home et création des itinéraires

--- src/pages/Accueil/Accueil.jsx
+++ src/pages/Accueil/Accueil.jsx
@@ -2,15 +2,16 @@ import bannerImg from "../../assets/images/IMG.png";
 import Banner from "../../components/Banner/Banner";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+
 import "./Accueil.css";
 
 const Accueil = () => {
   return (
     <>
       <Header />
-      <main className="main">
-        <Banner imageSrc={bannerImg} text="Chez vous, partout et ailleurs" />
-      </main>
+
+      <Banner imageSrc={bannerImg} text="Chez vous, partout et ailleurs" />
+
       <Footer />
     </>
   );
