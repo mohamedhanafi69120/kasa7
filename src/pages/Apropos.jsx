5bfc288b5466aa7a7968ad9995dcc1d8edd09bc3
Your Name <you@example.com>
2024-09-25 11:36:39 +0200

    mise en forme scss collapse

--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -2,6 +2,8 @@ import aboutBanner from "../assets/images/aproposIMG.png";
 import Banner from "../components/Banner/Banner";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
+import Collapse from "../components/Collapse/Collapse";
+import collapsesData from "../data/collapsesData.json";
 
 const Apropos = () => {
   return (
@@ -9,6 +11,13 @@ const Apropos = () => {
       <Header />
       <main className="main">
         <Banner imageSrc={aboutBanner} text="" className="banner-about" />
+        <section className="collapses-container">
+          {collapsesData.map((collapse) => (
+            <Collapse title={collapse.title} key={collapse.title}>
+              <p>{collapse.content}</p>
+            </Collapse>
+          ))}
+        </section>
       </main>
       <Footer />
     </>
